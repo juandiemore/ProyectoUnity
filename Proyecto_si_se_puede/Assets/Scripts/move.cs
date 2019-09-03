0c14203409083ec83a8b2ca97718b3308b881566
juan <juandiemore@gmail.com>
2019-09-02 22:23:23 -0500

    agregando caja

---
+++ Proyecto_si_se_puede/Assets/Scripts/move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,0.0f, MoveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
