75fee3efc422df79e7eae7849786433edc464ea7
juan <juandiemore@gmail.com>
2019-09-04 00:24:06 -0500

    npc message

---
+++ Proyecto_si_se_puede/Assets/Scripts/MsgNpc.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class MsgNpc : MonoBehaviour {
+    public string msg = "";
+
+    public string getMsg(){
+        return msg;
+    }
+}
---
+++ Proyecto_si_se_puede/Assets/Scripts/detection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class detection : MonoBehaviour
+{
+  [SerializeField] public GameObject msgPanel;
+  [SerializeField] Text mtext;
+        // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<Collider>();
+    }
+    void Start(){
+        msgPanel.SetActive(false);
+    }
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "npc"){
+        string msg = other.GetComponent<MsgNpc>().getMsg();
+        mtext.text = msg;
+         msgPanel.SetActive(true);
+        }
+    }
+
+        void OnTriggerExit(Collider other)
+    {
+        if(other.name == "npc"){
+         msgPanel.SetActive(false);
+        }
+    }
+}
