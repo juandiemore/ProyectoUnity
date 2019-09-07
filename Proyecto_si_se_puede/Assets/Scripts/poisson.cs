fbe50f2ac5b5d9bd4634dc3397e717c1b48b44f3
juan <juandiemore@gmail.com>
2019-09-06 20:23:58 -0500

    Revert "nivel 1 con gameover"
    This reverts commit b252413bf82ee107f9b58b371a5870cbd19e355c.

--- Proyecto_si_se_puede/Assets/Scripts/poisson.cs
+++ Proyecto_si_se_puede/Assets/Scripts/poisson.cs
@@ -8,7 +8,7 @@ public class poisson : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        pois = .8f;
+        pois = .5f;
     }
 
     // Update is called once per frame
@@ -17,8 +17,8 @@ public class poisson : MonoBehaviour
             other.GetComponent<vida_aj>().TakeDamage(pois);
             other.GetComponent<AjControl>().dolor();
             Rigidbody rb = other.GetComponent<Rigidbody>();
-            rb.AddForce(transform.up*35f);
-            rb.AddForce(transform.forward*-5f);
+            rb.AddForce(transform.up*40f);
+            rb.AddForce(transform.forward*-10f);
         }
         
     }
