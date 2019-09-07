3bac0d70d392d8ce70c7a3d3264410d41f74fb95
juan <juandiemore@gmail.com>
2019-09-06 19:28:45 -0500

    zombie

---
+++ Proyecto_si_se_puede/Assets/Scripts/Aj/brainCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brainCounter : MonoBehaviour
+{
+   [SerializeField] public Text puntos;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+       puntos = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        puntos = "X"+score;
+        score+=1;
+    }
+}
